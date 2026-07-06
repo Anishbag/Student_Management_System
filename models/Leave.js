090e263567f8ebd6a180027cd1f7304e03d394b9
Anish Bag <anishbag18@gmail.com>
2026-07-06 17:58:09 +0530

    change

--- models/Leave.js
+++ models/Leave.js
@@ -1,20 +1,34 @@
 import mongoose from "mongoose";
 
 const leaveSchema = new mongoose.Schema({
+
     employeeId:{
-        type : mongoose.Schema.Types.ObjectId,
-        ref:"Employee"
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Employee",
+        required:true
     },
 
-    fromDate : Date,
-    toDate : Date,
-    reason : String,
-    enum:[
-        "pending","Approved","Rejected"
-    ],
-    default : "pending" 
-},
-{timestamps : true}
-);
+    fromDate:{
+        type:Date,
+        required:true
+    },
+
+    toDate:{
+        type:Date,
+        required:true
+    },
+
+    reason:{
+        type:String,
+        required:true
+    },
+
+    status:{
+        type:String,
+        enum:["pending","approved","rejected"],
+        default:"pending"
+    }
+
+},{timestamps:true});
 
 export default mongoose.model("Leave",leaveSchema);
--- models/User.js
+++ models/User.js
@@ -1,32 +1,45 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
-    {
-        name:{
-            type : String,
-            require : true
-        },
-        email:{
-            type : String,
-            require : true,
-            unique : true
-
-        },
-        password:{
-            type: String,
-            require : true
-        },
-        role:{
-            type : String,
-            enum : ["employee","admin"],
-            default : "employee"
-        },
-        
-        profileImage: {
-                type: String,
-                default: ""
-        }
+{
+    name:{
+        type:String,
+        required:true
     },
-    {timestamps : true}
+
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+
+    password:{
+        type:String,
+        required:true
+    },
+
+    role:{
+        type:String,
+        enum:["admin","employee"],
+        default:"employee"
+    },
+
+    department:{
+        type:String,
+        required: true
+    },
+
+    joiningDate:{
+        type:Date,
+        default:Date.now
+    },
+
+    profileImage:{
+        type:String,
+        default:""
+    }
+},
+{timestamps:true}
 );
+
 export default mongoose.model("User",userSchema);
